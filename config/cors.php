264bc4c4743e4573c445ec632e5c82cfdd2f9d10
Ryaas Ishlah Ramadhan <ryaasishlah123@gmail.com>
2026-01-28 16:47:17 +0700

    update cors

--- config/cors.php
+++ config/cors.php
@@ -2,19 +2,30 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    */
+
+    // Semua endpoint API
     'paths' => [
         'api/*',
         'sanctum/csrf-cookie',
     ],
 
+    // Semua HTTP method
     'allowed_methods' => ['*'],
 
+    // Domain frontend yang diizinkan
     'allowed_origins' => [
-        'http://localhost:5173',
+        'http://localhost:5173', // React dev
+        'https://moccasin-crab-693879.hostingersite.com', // React production
     ],
 
     'allowed_origins_patterns' => [],
 
+    // Header yang boleh dikirim frontend
     'allowed_headers' => [
         'Content-Type',
         'Authorization',
@@ -23,10 +34,13 @@ return [
         'Origin',
     ],
 
+    // Header response yang boleh dibaca frontend (optional)
     'exposed_headers' => [],
 
+    // Cache preflight
     'max_age' => 0,
 
+    // Pakai Bearer token, bukan cookie
     'supports_credentials' => false,
 
 ];
