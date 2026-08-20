3b91cff4d6b3c9f823e7c899938b9d1c1e7defd0
git push <raulmahya11@gmail.com>
2026-08-20 20:45:48 +0700

    ok

--- database/migrations/2026_08_20_061027_drop_unused_hris_tables.php
+++ database/migrations/2026_08_20_061027_drop_unused_hris_tables.php
@@ -11,16 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::dropIfExists('job_openings');
-        Schema::dropIfExists('candidates');
-        Schema::dropIfExists('interview_schedules');
         Schema::dropIfExists('interview_evaluations');
+        Schema::dropIfExists('interview_schedules');
         Schema::dropIfExists('offer_letters');
         Schema::dropIfExists('background_checks');
         Schema::dropIfExists('talent_pool_entries');
+        Schema::dropIfExists('candidates');
+        Schema::dropIfExists('job_openings');
         Schema::dropIfExists('okrs');
-        Schema::dropIfExists('hr_service_requests');
         Schema::dropIfExists('hr_service_request_comments');
+        Schema::dropIfExists('hr_service_requests');
         Schema::dropIfExists('tasks');
         Schema::dropIfExists('review_360');
         Schema::dropIfExists('review360');
